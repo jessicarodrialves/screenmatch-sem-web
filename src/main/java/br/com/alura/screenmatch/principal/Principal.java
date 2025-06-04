a0b3a678496ceac161b26426c222f59baf3582d5
Jessica <jessica.rodrialves@outlook.com>
2025-06-04 09:11:37 -0300

    Desenvolvimento de estatisticas com Max/Min/Soma/Media

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -126,5 +126,16 @@ public class Principal {
                    Collectors.averagingDouble(Episodio::getAvaliacao)));
         System.out.println(avaliacoesPorTemporada);
 
+        // é um metodo que realiza estasticas (Soma/Max/Min/Media)
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e-> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
+
+
+
     }
 }
